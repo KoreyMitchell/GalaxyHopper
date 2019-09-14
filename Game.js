113775d50b058152c2ba12effbe5af4afdf480ce
Korey Mitchell <kmitchel@udel.edu>
2019-09-14 19:09:06 -0400

    Updates to mobile

--- Game.js
+++ Game.js
@@ -12,31 +12,17 @@ $(document).ready(function(e){
 
   var theElement = document.getElementById("body");
 
-  theElement.addEventListener("touchstart", handlerFunction, false);
   
-  function handlerFunction(event) {
-    $d.css("top", event.yloc + "px");
-    $d.css("left", event.xloc + "px");
-  }
 
   
 
   function gameLoop() {
     /////
-    document.querySelector('document.body').addEventListener('touchstart', f);
-    document.querySelector('document.body').addEventListener('touchend', f);
-    document.querySelector('document.body').addEventListener('touchmove', f);
-    
-    function f(ev){
-        console.alert( ev.touches, ev.type );
-
-        var xloc = parseFloat($d.css('left'));
-        var yloc = parseFloat($d.css('top'));
-        angle = Math.atan2(ev.touches.yloc, ev.touches.xloc)
-        xloc += 4*Math.cos(4.8+ angle * Math.PI/180)
-        yloc += 4*Math.sin(4.8+ angle * Math.PI/180)
-        $d.css("top", yloc + "px");
-        $d.css("left", xloc + "px");
+    theElement.addEventListener("touchstart", handlerFunction, false);
+  
+    function handlerFunction(event) {
+      $d.css("top", event.yloc + "px");
+      $d.css("left", event.xloc + "px");
     }
 
 
