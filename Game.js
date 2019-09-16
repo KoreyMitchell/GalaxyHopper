8fc26a0e9a3639e90d77a7804e1554a87f28a32a
Korey Mitchell <kmitchel@udel.edu>
2019-09-15 21:52:39 -0400

    Updates to shooting

--- Game.js
+++ Game.js
@@ -9,15 +9,66 @@ $(document).ready(function(e){
 
   var $d = $(".spaceShip");
   var angle = 0
- 
-  function gameLoop() {
+  $(window).height();
+  $(window).width();
+  var checkTime = 0;
+  var currentTime = new Date()
+
+  function areaCheck(){
+    var screeny = $(window).height();
+    var screenx = $(window).width();
+    var xloc = parseFloat($d.css('left'));
+    var yloc = parseFloat($d.css('top'));
+    
+    if(xloc > screenx){
+       $d.css("left", 0 + "px");
+    }
+    if(yloc > screeny){
+       $d.css("top", 0 + "px");
+    }
+    if(xloc < 0){
+      $d.css("left", screenx + "px");
+   }
+   if(yloc < 0){
+      $d.css("top", screeny + "px");
+   }
+  }
+
+  function angleCheck(angle){ // Check to keep 0 <= angle <= 360
     if (angle >= 360){
       angle = 0
     }
     else if (angle <= 0){
       angle = 359
     }
+  }
 
+ 
+
+  function fireBullet(xloc,yloc) {
+    var bulletCount = 1;
+    $("body").append($("<div>").addClass("bullet").css({ "left" : xloc + 60, 'top' : yloc + 20 }));
+    }
+
+  function update() {
+    $(".bullet").each(function() {
+        var bulletAngle = angle;
+        var oldLeft = $(this).offset().left;
+        var oldTop = $(this).offset().top;
+        oldLeft += 7*Math.cos(4.8+ angle * Math.PI/180)
+        oldTop += 7*Math.sin(4.8+ angle * Math.PI/180)
+        $(this).css("left", oldLeft);
+        $(this).css("top", oldTop);
+        
+      
+    });
+  }
+
+  var locked = false;
+ 
+  function gameLoop() {
+    areaCheck();
+    angleCheck(angle);
     if (keys[39]) {     //right
         angle+=5
         $d.css("transform", "rotate("+angle+"deg)")
@@ -26,14 +77,22 @@ $(document).ready(function(e){
         angle-=5
         $d.css("transform", "rotate("+angle+"deg)")
     }
+
     if (keys[40]) { //down
-        // var xloc = parseFloat($d.css('left'));
-        // var yloc = parseFloat($d.css('top'));
-        // xloc += Math.cos(angle)
-        // yloc += Math.sin(angle)
-        // $d.css("top", yloc + 'px');
-        // $d.css("left", xloc + 'px');
-    }
+        if(!locked){
+          
+        locked = true;
+        
+        var xloc = parseFloat($d.css('left'));
+        var yloc = parseFloat($d.css('top'));
+        
+        fireBullet(xloc,yloc);
+       
+        var i = setInterval(update ,50);
+        setTimeout(function( ) { clearInterval( i );}, 5000);
+        setTimeout(function( ) { locked = false;}, 250);
+        }
+  }
     else if (keys[38]) { //up
       forward = true;
       var xloc = parseFloat($d.css('left'));
@@ -44,9 +103,12 @@ $(document).ready(function(e){
       $d.css("left", xloc + "px");
     }
     
-    setTimeout(gameLoop, 10);
+    setTimeout(gameLoop, 15);
 
   }
+  
+
+
   gameLoop();
     
   $(document).focus();
