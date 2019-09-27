9b206acd9dd08c1616a75914ee198133d3aa24cf
Korey Mitchell <kmitchel@udel.edu>
2019-09-27 14:38:33 -0400

    Asteroids collision added

--- Game.js
+++ Game.js
@@ -1,149 +1,288 @@
+/**
+ * Enums for directions
+ */
+const direction = {
+	LEFT: 37,
+	RIGHT: 39,
+	UP: 38,
+	SPACE: 32,
+}
+
+/**
+ * Enums for game settings
+ */
+const game = {
+	MAX_SPEED: 7,
+	ROTATE_ANGLE: 5,
+	DECELERATION: 0.98,
+	SMOOTH_ACCELERATION_CONST: 0.05,
+	SHIP_HEIGHT: 100,
+	SHIP_WIDTH: 100,
+	MAX_ASTEROID_CNT: 10,
+	SCREEN_WIDTH: screen.width,
+	SCREEN_HEIGHT: screen.height,
+}
+
+$(document).ready(function () {
+	unloadScrollBars();
+	$("#StartButton").click(function () {
+		$("#splashscreen").fadeOut(1000);
+		$(".spaceShip").show();
+		$("body").show();
+		createAsteroidList();
+	});
+
+	let keys = {}; //dictionary to keep track of key presses
+	document.addEventListener("keydown", function (event) {
+		if (event.keyCode == direction.RIGHT || event.keyCode == direction.LEFT || event.keyCode == direction.SPACE || event.keyCode == direction.UP) {
+			keys[event.keyCode] = true;
+		}
+	});
+
+	document.addEventListener("keyup", function (event) {
+		if (event.keyCode == direction.RIGHT || event.keyCode == direction.LEFT || event.keyCode == direction.SPACE || event.keyCode == direction.UP) {
+			keys[event.keyCode] = false;
+		}
+	});
+
+	const ship = document.getElementById("spaceShip");
+	resetShipPosition();
+
+	function resetShipPosition() {
+		ship.style.left = (game.SCREEN_WIDTH * 0.5) + "px";
+		ship.style.top = (game.SCREEN_HEIGHT * 0.5) + "px";
+	}
+
+	/**
+	 * populate the screen with asteroids
+	 * WHEN LAUNCH
+	 */
+
+	function createAsteroidList() {
+		for (let i = 0; i < game.MAX_ASTEROID_CNT; i++) {
+			createAsteroid();
+		}
+	}
+
+	function createAsteroid() {
+		let randomAngle = 0;
+		let randomX = 0;
+		let randomY = 0;
+
+		randomAngle = Math.floor(Math.random() * 361);
+		randomX = -1 * Math.random(); //Math.floor(Math.random() * (game.SCREEN_WIDTH + 1));
+		randomY = -1 * Math.random(); //Math.floor(Math.random() * (game.SCREEN_HEIGHT + 1));
+
+		let asteroid_img = `<img id=${randomAngle} class='asteroid' src='assets/asteroid.png' style='left: ${randomX}px; top: ${randomY}px; width: ${100}px; height: ${100}px'>`
+		document.getElementById("asteroidListDiv").innerHTML += asteroid_img;
+	}
+
+	let angle = 0;
+	let accelerationX = 0;
+	let accelerationY = 0;
+	let velocityX = 0;
+	let velocityY = 0;
+
+	let bulletCount = 0;
+	/**
+	 * check for ship going out of screen 
+	 */
+	function areaCheck() {
+		let xloc = parseFloat(ship.style.left);
+		let yloc = parseFloat(ship.style.top);
+
+		if (xloc > game.SCREEN_WIDTH) {
+			ship.style.left = -game.SCREEN_WIDTH + "px";
+		} else if (xloc < -game.SHIP_WIDTH) {
+			ship.style.left = game.SCREEN_WIDTH + "px";
+		}
+		if (yloc > game.SCREEN_HEIGHT - 50) {
+			ship.style.top = -game.SHIP_HEIGHT + "px";
+		} else if (yloc < -game.SHIP_HEIGHT) {
+			ship.style.top = game.SCREEN_HEIGHT + -game.SHIP_HEIGHT + "px";
+		}
+	}
+
+	/**
+	 * adjust angle accordingly
+	 */
+	function angleCheck() {
+		if (angle > 360) {
+			angle = 0;
+		}
+		if (angle < 0) {
+			angle = 360;
+		}
+	}
+
+	/**
+	 * remove scrollbar
+	 */
+	function unloadScrollBars() {
+		document.documentElement.style.overflow = 'hidden';  // firefox, chrome
+		document.body.scroll = "no"; // ie only
+	}
+
+	/**
+	 * create an instance of bullet
+	 * and display it dynamically
+	 * @param {number} x coordinate
+	 * @param {number} y coordinate
+	 * @param {number} ang - angle to be fire
+	 */
+	function fireBullet(x, y, ang) {
+		bulletCount++;
+		let bul_img = `<img id=${ang} class='bullet' src='assets/bullet.png' style='left: ${x + 60}px; top: ${y + 20}px; width: ${16}px; height: ${40}px; transform: rotate(${ang}deg)'>`
+		document.getElementById("bulletListDiv").innerHTML += bul_img;
+	}
+
+	let locked = false;
+
+	/**
+	 * update position of space ship based on 
+	 * acceleration and velocity
+	 */
+	function updatePosition() {
+		let xloc = parseFloat(ship.style.left);
+		let yloc = parseFloat(ship.style.top);
+
+		// enforce speed limit
+		velocityX = (velocityX >= game.MAX_SPEED) ? game.MAX_SPEED : velocityX + accelerationX;
+		velocityY = (velocityY >= game.MAX_SPEED) ? game.MAX_SPEED : velocityY + accelerationY;
+
+		xloc += velocityX;
+		yloc += velocityY;
+		ship.style.left = xloc + "px";
+		ship.style.top = yloc + "px";
+	}
+
+	/**
+	 * update positions of fired bullets
+	 */
+	function updateBullets() {
+		let bList = document.getElementsByClassName("bullet");
+		for (let bullet of bList) {
+			const angle = bullet.id;
+			let bullet_x = parseFloat(bullet.style.left);
+			let bullet_y = parseFloat(bullet.style.top);
+			bullet_x += 10 * Math.cos(((angle - 90) * Math.PI) / 180);
+			bullet_y += 10 * Math.sin(((angle - 90) * Math.PI) / 180);
+			bullet.style.left = bullet_x + "px";
+			bullet.style.top = bullet_y + "px";
+			if (bullet_x > game.SCREEN_WIDTH || bullet_y < 0 || bullet_y > game.SCREEN_HEIGHT || bullet_y < 0) {
+				bullet.remove();
+			}
+		}
+	}
+
+	/**
+	 * update positions of moving asteroids
+	 */
+	function updateAsteroids() {
+		let asteroidList = document.getElementsByClassName("asteroid");
+		for (let asteroid of asteroidList) {
+			const angle = asteroid.id;
+			let asteroid_x = parseFloat(asteroid.style.left);
+			let asteroid_y = parseFloat(asteroid.style.top);
+			asteroid_x += 3 * Math.cos(((angle - 90) * Math.PI) / 180);
+			asteroid_y += 3 * Math.sin(((angle - 90) * Math.PI) / 180);
+			asteroid.style.left = asteroid_x + "px";
+			asteroid.style.top = asteroid_y + "px";
+
+			if (asteroid_x > game.SCREEN_WIDTH) {
+				asteroid.style.left = "0px";
+			} else if (asteroid_x < 0) {
+				asteroid.style.left = game.SCREEN_WIDTH + "px";
+			}
+			if (asteroid_y > game.SCREEN_HEIGHT) {
+				asteroid.style.top = "0px";
+			} else if (asteroid_y < 0) {
+				asteroid.style.top = game.SCREEN_HEIGHT + "px";
+			}
+
+			// if (asteroid_x > game.SCREEN_WIDTH || asteroid_y < 0 || asteroid_y > game.SCREEN_HEIGHT || asteroid_x < 0) {
+			// 	asteroid.remove();
+			// 	createAsteroid();
+			// }
+		}
+	}
+
+	/**
+	 * check if ship or bullets collides with asteroids
+	 */
+	function collisionDetect() {
+		let asteroidList = document.getElementsByClassName("asteroid");
+		for (let asteroid of asteroidList) {
+			let bList = document.getElementsByClassName("bullet");
+			for (let bullet of bList) {
+				if(isCollide(asteroid, bullet)){
+					asteroid.remove();
+					// createAsteroid();
+				}
+	}
+}
+	}
+
+	function isCollide(asteroid, bullet) {
+		let asteroid_x = parseFloat(asteroid.style.left);
+		let asteroid_y = parseFloat(asteroid.style.top);
+		let asteroid_w = parseFloat(asteroid.style.width);
+		let asteroid_h = parseFloat(asteroid.style.height);
+		let bullet_x = parseFloat(bullet.style.left);
+		let bullet_y = parseFloat(bullet.style.top);
+		let bullet_w = parseFloat(bullet.style.width);
+		let bullet_h = parseFloat(bullet.style.height);
+		console.log(`asteroid_x: ${asteroid_x} asteroid_y: ${asteroid_y} bullet_x: ${bullet_x} bullet_y: ${bullet_y}`);
+		console.log(`asteroid_w: ${asteroid_w} asteroid_h: ${asteroid_h} bullet_w: ${bullet_w} bullet_h: ${bullet_h}`);
+		return !(
+			((asteroid_y + asteroid_h) < (bullet_y)) ||
+			(asteroid_y > (bullet_y + bullet_h)) ||
+			((asteroid_x + asteroid_w) < bullet_x) ||
+			(asteroid_x > (bullet_x + bullet_w))
+		);
+	}
 
 
-$(document).ready(function(e){
-  $( "#StartButton" ).click(function() {
-    $( "#splashscreen" ).fadeOut(1000);
-    $(".spaceShip").show();
-    $("body").show();
-  });
 
-  
+	function gameLoop() {
+		angleCheck();
+		areaCheck();
 
-  $('body').on('touchstart', function() { 
-    $('.spaceShip').css("transform", "rotate("+angle+"deg)")
-  }); 
-
-
-  var keys = {};
-  $(document).keydown(function(event){
-    keys[event.which] = true;
-  }).keyup(function(event){
-    delete keys[event.which];
-  });
-
-  var $d = $(".spaceShip");
-  var angle = 0
-  $(window).height();
-  $(window).width();
-  var checkTime = 0;
-  var bulletCount = 0;
-  
-  
-
-  
-
-  function areaCheck(){
-    var screeny = $(window).height();
-    var screenx = $(window).width();
-    var xloc = parseFloat($d.css('left'));
-    var yloc = parseFloat($d.css('top'));
-    
-    if(xloc > screenx){
-       $d.css("left", 0 + "px");
-    }
-    if(yloc > screeny){
-       $d.css("top", 0 + "px");
-    }
-    if(xloc < 0){
-      $d.css("left", screenx + "px");
-   }
-   if(yloc < 0){
-      $d.css("top", screeny + "px");
-   }
-  }
-
-  function angleCheck(angle){ // Check to keep 0 <= angle <= 360
-    if (angle >= 360){
-      angle = 0
-    }
-    else if (angle <= 0){
-      angle = 359
-    }
-  }
-
-  // function fireLaser() {
-  //   let laser = createLaserElement()
-  //   mainPlayArea.appendChild(laser)
-  //   // let laserSFX = new Audio('audio/laser-sfx.m4a')
-  //   // laserSFX.play()
-  //   moveLaser(laser)
-  // }
-  // const shooter = document.getElementsByClassName("spaceShip")
-  // function createLaserElement() {
-  //   let xPosition = parseInt(window.getComputedStyle(shooter).getPropertyValue('left'))
-  //   let yPosition = parseInt(window.getComputedStyle(shooter).getPropertyValue('top'))
-  //   let newLaser = document.createElement('img')
-  //   newLaser.src = 'images/bullet.png'
-  //   newLaser.classList.add('bullet')
-  //   newLaser.style.left = `${xPosition}px`
-  //   newLaser.style.top = `${yPosition - 10}px`
-  //   return newLaser
-  // }
-
-  // function moveLaser(laser) {
-  //   let laserInterval = setInterval(() => {
-  //     let xPosition = parseInt(laser.style.left)
-  //     // let monsters = document.querySelectorAll(".monster")
-  //     // monsters.forEach(monster => {
-  //     //   if (checkLaserCollision(laser, monster)) {
-  //     //     // let explosion = new Audio('audio/explosion.m4a')
-  //     //     // explosion.play()
-  //     //     monster.src = "images/bullet.png"
-  //     //     monster.classList.remove("monster")
-  //     //     monster.classList.add("dead-monster")
-  //     //     scoreCounter.innerText = parseInt(scoreCounter.innerText) + 100
-  //     //   }
-  //     // })
-  //     if (xPosition === 340) {
-  //       laser.remove()
-  //     } else {
-  //       laser.style.left = `${xPosition + 4}px`
-  //     }
-  //   }, 10)
-  // }
-
-  
- 
-  function gameLoop() {
-    
-    areaCheck();
-    angleCheck(angle);
-    if (keys[39]) {     //right
-        angle+=5
-        $d.css("transform", "rotate("+angle+"deg)")
-    }
-    else if (keys[37]) { //left
-        angle-=5
-        $d.css("transform", "rotate("+angle+"deg)")
-    }
-
-    if (keys[32]) { //down
-        fireLaser();
-     
-      
-        
-       
-        var i = setInterval(update ,50);
-        setTimeout(function( ) { clearInterval( i );}, 5000);
-       
-        
-  }
-    else if (keys[38]) { //up
-      forward = true;
-      var xloc = parseFloat($d.css('left'));
-      var yloc = parseFloat($d.css('top'));
-      xloc += 7*Math.cos(4.8+ angle * Math.PI/180)
-      yloc += 7*Math.sin(4.8+ angle * Math.PI/180)
-      $d.css("top", yloc + "px");
-      $d.css("left", xloc + "px");
-    }
-    
-    setTimeout(gameLoop, 15);
-
-  }
-  
-
-  gameLoop();
-    
-  $(document).focus();
+		if (keys[direction.RIGHT]) {
+			angle = angle + game.ROTATE_ANGLE;
+			ship.style.transform = "rotate(" + angle + "deg)";
+		}
+		if (keys[direction.LEFT]) {
+			angle = angle - game.ROTATE_ANGLE;
+			ship.style.transform = "rotate(" + angle + "deg)";
+		}
+		if (keys[direction.SPACE]) {
+			if (!locked) {
+				locked = true;
+				var snd = new Audio("assets/Blast.mp3");
+				snd.play();
+				let xloc = parseFloat(ship.style.left);
+				let yloc = parseFloat(ship.style.top);
+				fireBullet(xloc, yloc, angle);
+				setTimeout(function () {
+					locked = false;
+				}, 250);
+			}
+		}
+		if (keys[direction.UP]) {
+			accelerationX = game.SMOOTH_ACCELERATION_CONST * Math.cos(4.8 + (angle * Math.PI) / 180);
+			accelerationY = game.SMOOTH_ACCELERATION_CONST * Math.sin(4.8 + (angle * Math.PI) / 180);
+		}
+		if (!keys[direction.UP]) {
+			// deceleration
+			velocityX *= game.DECELERATION;
+			velocityY *= game.DECELERATION;
+		}
+		collisionDetect();
+		updatePosition();
+		updateBullets();
+		updateAsteroids();
+		requestAnimationFrame(gameLoop);
+	}
+	gameLoop();
 });
